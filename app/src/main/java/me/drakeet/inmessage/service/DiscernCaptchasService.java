8a02591d8789ffa93d17c692e541c43ff04c97f5
shengkun <444341703@qq.com>
2015-06-30 14:02:32 +0800

    放弃使用BUS发送吐司弹出事件。修改使其在onStartCommand中调用。

--- app/src/main/java/me/drakeet/inmessage/service/DiscernCaptchasService.java
+++ app/src/main/java/me/drakeet/inmessage/service/DiscernCaptchasService.java
@@ -28,17 +28,18 @@ public class DiscernCaptchasService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Bundle bundle = intent.getBundleExtra("bundle");
-        Message message = (Message)bundle.getSerializable("message");
+        if(intent != null) {
+            Bundle bundle = intent.getBundleExtra("bundle");
+            Message message = (Message) bundle.getSerializable("message");
 
-        if(message.getCaptchas() != null) {
-            ClipboardUtils.putTextIntoClipboard(DiscernCaptchasService.this, message.getCaptchas());
-            // 弹两遍，加长时间。
-            ToastUtils.showLong(String.format(getResources().getString(R.string.tip), message.getCaptchas()));
-            ToastUtils.showLong(String.format(getResources().getString(R.string.tip), message.getCaptchas()));
-            NotificationUtils.showMessageInNotificationBar(DiscernCaptchasService.this, message);
+            if (message.getCaptchas() != null) {
+                ClipboardUtils.putTextIntoClipboard(DiscernCaptchasService.this, message.getCaptchas());
+                // 弹两遍，加长时间。
+                ToastUtils.showLong(String.format(getResources().getString(R.string.tip), message.getCaptchas()));
+                ToastUtils.showLong(String.format(getResources().getString(R.string.tip), message.getCaptchas()));
+                NotificationUtils.showMessageInNotificationBar(DiscernCaptchasService.this, message);
+            }
         }
-
         return START_STICKY;
     }
 }
